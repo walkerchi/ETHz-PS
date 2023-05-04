6ad5e42a4df7a9405b8b036fa86fc84837c6cd12
walkerchi <walkerchi@foxmail.com>
2023-05-04 12:40:37 +0200

    add homepage

--- presentations/progress-report/src/App.js
+++ presentations/progress-report/src/App.js
@@ -1,28 +1,58 @@
 import React from 'react'
 import { useEffect } from 'react'
-import Revealjs from "reveal.js"
+import Reveal from "reveal.js"
 import RevealMarkdown from "reveal.js/plugin/markdown/markdown.esm.js"
 import RevealMath from "reveal.js/plugin/math/math.esm.js"
+import styles from "./App.module.css"
 
+import ETHLogo from "./svg/ethz.js"
 import "reveal.js/dist/reveal.css"
 import "reveal.js/dist/theme/white.css"
-import "katex/dist/katex.min.css"
+// import "katex/dist/katex.min.css"
 
-import MD from "./README.md"
+import markdown from "./README.md"
 function App() {
   useEffect(()=>{
-    Reveal.initialize({
+    
+    var deck = new Reveal({
+      katex: {
+        // local:'node_modules/katex',
+        version:"0.16.6",
+        delimiters: [
+          {left: '$$', right: '$$', display: true},
+          {left: '$', right: '$', display: false},
+          {left: '\\(', right: '\\)', display: false},
+          {left: '\\[', right: '\\]', display: true}
+       ],
+       extensions:['mhchem'],
+       ignoredTags: ['script', 'noscript', 'style', 'textarea', 'pre']
+      },
       plugins: [ 
         RevealMarkdown,
         RevealMath.KaTeX ]
     });
-  })
+    deck.initialize()
+
+  },[])
 
   return (
-    <div className="App">
-      <section data-markdown={MD}>
-
-      </section>
+    <div className={styles.container}>
+      <div className="reveal">
+        <div className="slides">
+          <section data-markdown={markdown}
+            data-separator="---"
+            data-separator-vertical="==="
+            >
+          
+          </section>
+        </div>
+      </div>
+      <div className={styles.logo}>
+        <ETHLogo/>
+      </div>
+      <div className={styles.me}>
+        Mingyuan Chi
+      </div>
     </div>
   );
 }
